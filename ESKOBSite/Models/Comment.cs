fc1e6a5aea972d8fab33c8881821981d87e7d6c4
Arni Bent Thráinsson <arni0114@easv365.dk>
2020-11-25 16:16:40 +0100

    Add comment functionality

---
+++ ESKOBSite/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ESKOBSite.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Body { get; set; }
+        public Idea Idea { get; set; }
+        //public Task Task {get; set; }
+        //public Manager Author { get; set; }
+    }
+}
