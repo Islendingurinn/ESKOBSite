68ddf48e76ef37d5b4e3071e11414f4e63136a2a
Arni Bent Thráinsson <arni0114@easv365.dk>
2020-11-27 08:09:13 +0100

    Add comment mention functionality + libraries

---
+++ ESKOBSite/Models/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ESKOBSite.Models
+{
+    public class Manager
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Displayname { get; set; }
+    }
+}
